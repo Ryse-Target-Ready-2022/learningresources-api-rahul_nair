1fbd4d82bb218e494211a2c4526672c9b6bf2675
ryuzaki07 <rahulnair.rn34@gmail.com>
2022-07-03 22:08:48 +0530

    Added Hello World

---
+++ src/main/java/com/tgt/rysetti/learningresourcesapi/LearningresourcesapiApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetti.learningresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesapiApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetti/learningresourcesapi/LearningresourcesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetti.learningresourcesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
